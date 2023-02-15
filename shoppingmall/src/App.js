6df5c540a502b5da4dfbf0ddf74cfaf367c02bb1
정제우 <wpdn3825@naver.com>
2023-02-15 23:54:29 +0900

    feat : nav, main benner 추가

---
+++ shoppingmall/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <container>
+          <div className="title">WellcomeShop</div>
+          <nav>
+            <a>HOME</a>
+            <a>CART</a>
+          </nav>
+        </container>
+      </nav>
+
+      <div className="main_benner">
+        <div className="flexbox">
+          <img src="helloworld.png"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
